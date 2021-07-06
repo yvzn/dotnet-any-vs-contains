50f1606f773c18ce3615375d10082737142456ad
Yvan Razafindramanana <mail@ludeo.net>
2021-07-06 07:49:33 +0200

    Add list specific benchmarks

--- benchmark/AnyVsContainsBenchmark/Program.cs
+++ benchmark/AnyVsContainsBenchmark/Program.cs
@@ -7,6 +7,8 @@ namespace AnyVsContainsBenchmark
         static void Main(string[] args)
         {
             BenchmarkRunner.Run<Benchmarks>();
+            BenchmarkRunner.Run<BenchmarkForLists>();
+            BenchmarkRunner.Run<BenchmarksForCollections>();
         }
     }
 }
